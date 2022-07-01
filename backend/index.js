1cb1a740e2e781b540d98133ddafff1922e506c5
Bella <tuyisenge1109@gmail.com>
2022-07-01 15:49:20 +0200

    deploy

--- backend/index.js
+++ backend/index.js
@@ -640,6 +640,20 @@ QRCode.toDataURL(stringdata, function (err, code) {
 })
 
   });
+// _________________________________________sending bookings______________________________
+  app.get('/api/bookings', async (req, res) => {
+	res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );
+	const rows= await process.postgresql.query('SELECT * FROM booking;');
+	res.json(rows);
+  });
+
+//___________________________________ Sending a single visitor ________________________________________________
+app.get('/api/bookings/:id', async (req, res) => {
+	res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );
+	const pk=req.params['id'];
+	const rows = await process.postgresql.query('SELECT * FROM booking WHERE id=$1', [pk]);
+	res.json(rows);
+  });
 
 // ___________________________________________pdf________________________________________________________________
 app.get('/api/pdf/visits', async(req,res)=>{
