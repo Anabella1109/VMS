88c7ea97637a6311eb651c0f9d03d557168fb427
Bella <tuyisenge1109@gmail.com>
2022-07-16 22:02:12 +0200

    deploy

--- backend/index.js
+++ backend/index.js
@@ -499,7 +499,7 @@ app.post('/api/checkin', async (req, res) => {
 		const getvisit= ()=>{for(entry in obj){
 			// console.log(entry);
 		
-			return JSON.parse(entry);
+			return entry;
 		}
 	};
 		const visit=getvisit();
