5af3088c3f2027d823643b4c64a7bba288dbbbd5
Bella <tuyisenge1109@gmail.com>
2022-07-16 11:42:50 +0200

    deploy

--- backend/index.js
+++ backend/index.js
@@ -63,7 +63,8 @@ const corsOptions ={
 // const router = express.Router();
 app.use("/public",express.static(__dirname+'/public'));
 app.use(cors());
-app.use(bodyParser.json({type: '*/*'}));
+// app.use(bodyParser.json({type: '*/*'}));
+app.use(require('connect').bodyParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
