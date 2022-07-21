58de9e7c0db94d651cddd8bb1e8f78fdccfabb1c
Bella <tuyisenge1109@gmail.com>
2022-07-21 14:58:50 +0200

    deploy

--- backend/index.js
+++ backend/index.js
@@ -1617,7 +1617,9 @@ let UploadCsvDataToMyDatabase= (filePath)=>{
             let rows= process.postgresql.query(`SELECT * FROM hosts`); 
 			for(item in rows){
 				emails.push(item.email_id);
+				console.log(item.email_id)
 			};
+			console.log(emails);
 
 			let query =   "INSERT INTO hosts ( name, email_id, mobile_no, department,password) VALUES ($1, $2, $3, $4,$5)";
 			try {
